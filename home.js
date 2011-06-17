9772330ecf23d6556e4a4f67136557f918bcb558
Engel A. Sanchez <engel_sanchez@yahoo.com>
2011-06-16 22:24:01 -0400

    Adding little fancy rotating list of "things I love"

--- home.js
+++ home.js
@@ -53,6 +53,31 @@ $(function(){
 			$(btnId).click();
 		
 	}
-  var homebg = Raphael("homebg", 400, 400);
-  homebg.circle(0, 0, 50).attr({"stroke":"#bbf", "stroke-width":2});
+	var homebg = Raphael("homebg", 400, 400);
+	homebg.circle(0, 0, 50).attr({"stroke":"#bbf", "stroke-width":2});
+
+	// Initial placement for love items
+	var center = { x: 329, y: 100 };
+	var r = {x:200, y:60 };
+	var yfactor = r.y / (1.0 * r.x);
+	var loveItems = $(".love-item");
+	var nLoveItems = loveItems.length;
+	var angleStep = 360 / nLoveItems;
+	var initAngle = 0;
+	var lx = 100 / 2.0, ly = 40 / 2.0;
+  
+	var updateFn = function(){
+		var angle = initAngle;
+		loveItems.each(function(){
+			var x = center.x + Math.sin(Math.PI * angle / 180) * r.x - lx;
+			var y = center.y + yfactor * ( Math.cos(Math.PI * angle / 180) * r.x ) - ly;
+			angle += angleStep;
+			$(this).css({top: y + "px", left: x + "px" });
+		});
+		initAngle += .2;
+		initAngle = initAngle % 360;
+		setTimeout(updateFn, 80);
+	};
+	
+	updateFn();
 });
